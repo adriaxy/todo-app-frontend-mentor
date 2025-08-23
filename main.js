f2949c312b3cbe780ba79479c7f365981892475f
adriaxy <adrisup959@gmail.com>
2025-08-23 12:22:45 +0200

    Add animation to theme button toggle

--- main.js
+++ main.js
@@ -4,6 +4,36 @@ const interactiveSection = document.querySelector('.interactive-content');
 const divFooter = document.createElement('div');
 divFooter.className = 'div-footer';
 
+//Color theme
+const btnTheme = document.querySelector('.btn-theme');
+const body = document.querySelector('body');
+const darkBtn = document.querySelector('.dark-theme-svg')
+const lightBtn = document.querySelector('.light-theme-svg');
+let isAnimating = false;
+
+
+//Theme button
+btnTheme.addEventListener('click', (e)=> {
+  e.preventDefault();
+  toggleTheme();
+  if(isAnimating) return;
+  isAnimating = true;
+
+  const active = !darkBtn.classList.contains('hide-above') ? darkBtn : lightBtn;
+  const next = active === darkBtn ? lightBtn : darkBtn;
+
+  active.classList.add('move-down');
+  next.classList.remove('move-down', 'hide-above');
+
+  active.addEventListener('transitionend', ()=> {
+    active.classList.remove('move-down');
+    active.classList.add('hide-above');
+    isAnimating = false;
+  }, {once:true});
+})
+
+
+//Interactive section div repositioning
 function repositionDiv() {
   if (window.innerWidth < 590) {
     if(!interactiveSection.contains(divFooter)){
@@ -16,8 +46,15 @@ function repositionDiv() {
   }
 }
 
-// Se ejecuta al cargar la página
+
 window.addEventListener('DOMContentLoaded', repositionDiv)
-// Se ejecuta cuando se cambia el tamaño de la ventana
 window.addEventListener('resize', repositionDiv);
+
+
+function toggleTheme() {
+  const current = body.dataset.theme;
+  body.dataset.theme = current === 'dark' ? 'light' : 'dark';
+}
+
+
 
