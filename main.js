fd2a7c29249701de7aa6e08b01027134dccbeb97
adriaxy <adrisup959@gmail.com>
2025-08-25 17:18:11 +0200

    Add generate new element li to the list + add button to remove item from the list

--- main.js
+++ main.js
@@ -1,15 +1,98 @@
-const divSummary = document.querySelector('.todo-summary');
-const stateButtons = document.querySelector('.state-buttons');
-const interactiveSection = document.querySelector('.interactive-content');
+const $ = selector => document.querySelector(selector);
+const $$ = selector => document.querySelectorAll(selector);
+const $id = id => document.getElementById(id);
+
+
+const divSummary = $('.todo-summary');
+const stateButtons = $('.state-buttons');
+const interactiveSection = $('.interactive-content');
 const divFooter = document.createElement('div');
 divFooter.className = 'div-footer';
 
-//Color theme
-const btnTheme = document.querySelector('.btn-theme');
-const body = document.querySelector('body');
-const darkBtn = document.querySelector('.dark-theme-svg')
-const lightBtn = document.querySelector('.light-theme-svg');
+const btnTheme = $('.btn-theme');
+const body = $('body');
+const darkBtn = $('.dark-theme-svg')
+const lightBtn = $('.light-theme-svg');
 let isAnimating = false;
+const btnDelete = $('.delete');
+
+const form = $('.todo-form');
+const input = $id('todo-input');
+const emptyList = $('.empty-list');
+const listContainer = $('.list-container');
+let modes = {
+    all: 'all',
+    active: 'active',
+    completed: 'completed'
+  }
+const li = $('.li')
+
+const listItems = []
+
+function createNewItem(inputElement){
+  listItems.push(
+    {
+      text: inputElement.value,
+      completed: false,
+      id: Date.now()
+    }
+  )
+};
+
+//Form
+form.addEventListener('submit', (e)=> {
+  e.preventDefault();
+  if(!input.value) return; 
+
+  if(input.value){
+    emptyList.style.display='none';
+    createNewItem(input);
+
+    const lastItem = listItems.length-1;
+    listContainer.prepend(addItemToList(listItems[lastItem].id, listItems[lastItem].text))
+    input.value = ''
+  }
+})
+
+listContainer.addEventListener('click', (e)=> {
+  if(e.target.tagName === 'BUTTON'){
+    const btnId = Number(e.target.dataset.id);
+    const index = listItems.findIndex((element) => element.id === btnId);
+    if (index !== -1) listItems.splice(index, 1)
+    e.target.closest('li').remove();
+  } 
+})
+
+// listContainer.addEventListener('click', (e)=> {
+//   if(e.target.tagName === 'BUTTON'){
+//     btnId = Number(e.target.dataset.id);
+//     console.log(listItems)
+//     listItems.forEach((element, index)=> {
+//       const objectValues = Object.values(element);
+//       if(objectValues.find((element) => element === btnId))listItems.splice(index, 1);
+//     })
+//     console.log(listItems)
+//     e.target.closest('li').remove();
+//         console.log(listItems)
+//   } 
+// })
+
+
+function addItemToList(id, text){
+  const li = document.createElement('li');
+  li.classList.add('li');
+  li.id = id;
+  li.innerHTML = `<label for="input-${id}" class="label-li">
+        <input type="checkbox" id="input-${id}">
+        <span class="custom-check round">
+          <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#000" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+        </span>
+        <span class="todo-text">${text}</span>
+      </label>
+      <button type="button" class="delete" data-id="${id}">
+      </button>`
+  return li
+}
 
 
 //Theme button
@@ -55,4 +138,13 @@ function toggleTheme() {
   const current = body.dataset.theme;
   body.dataset.theme = current === 'dark' ? 'light' : 'dark';
 }
+
+
+
+
+
+
+
+
+
 
